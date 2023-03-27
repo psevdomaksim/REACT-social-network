4b0862ac09d847d9d27f20bcda6816bfb48ddb96
psevdomaksim <varmax2004@gmail.com>
2023-03-28 02:28:24 +0300

    add posts, re-render, Context

--- src/components/Posts/PostList.jsx
+++ src/components/Posts/PostList.jsx
@@ -2,58 +2,76 @@ import "../../App.css";
 import "../css/Content.css";
 import { Image, Form, Button } from "react-bootstrap";
 import Post from "./Post";
-import { Posts } from "../STATE";
 import { useState } from "react";
 import { useEffect } from "react";
-
+import { addPost } from "../../controllers/PostsController";
+import { useContext } from "react";
+import { Context } from "../../index";
 
 const PostList = () => {
   const [posts, setPosts] = useState([]);
+
+  const Posts = useContext(Context);
+
+  const getRandomNumber = () => {
+    let min = 1000;
+    let max = 9999;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
   useEffect(() => {
     setPosts(Posts);
   }, []);
 
   const [post, setPost] = useState({
+    id: null,
     author: "",
     text: "",
   });
 
-
   const clear = () => {
-    setPost({text: "" });
+    setPost({id: null, author:"", text: ""});
   };
 
   const onChange = (event) => {
     if (event.target.id == "text") {
-      setPost({ ...post, author: "maksos", text: event.target.value });
+      setPost({
+        ...post,
+        id: getRandomNumber(),
+        author: "maksos",
+        text: event.target.value,
+      });
     }
   };
 
-  let addPost = () => {
-    setPosts([...posts, post]);
-    clear()
+  const addPostFunc = () => {
+    console.log(post)
+    if (post.text != "") {
+      setPosts([...posts, post]);
+      addPost(post);
+      clear();
+    }else{
+      return;
+    }
   };
 
-
-  console.log(...posts);
   return (
     <>
       <div className="main-posts">
         <h3>My posts</h3>
         <Form>
-          <Form.Control as="textarea" id="text" rows={2} onChange={onChange} />
+          <Form.Control as="textarea" id="text" rows={2} value={post.text} onChange={onChange} />
         </Form>
         <Button
           style={{ marginTop: "10px" }}
           variant="success"
-          onClick={addPost}
+          onClick={addPostFunc}
         >
           Send
         </Button>
       </div>
 
-      {Posts.map((post) => (
+      {posts.map((post) => (
         <Post author={post.author} text={post.text} />
       ))}
     </>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { createContext } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Posts } from "../src/components/STATE";
 
+export const Context = createContext(null);
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
+  <Context.Provider value={Posts}>
+    <App />,
+  </Context.Provider>,
+
+  document.getElementById("root")
 );
